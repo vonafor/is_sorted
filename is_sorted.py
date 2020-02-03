e9cc8da3d2b71b90afb87ca641c0c62b6c5f7ca9
Yan Mitrofanov <vonafor@users.noreply.github.com>
2020-02-04 00:31:50 +0300

    support multi keys sorting with custom order

--- is_sorted.py
+++ is_sorted.py
@@ -1,28 +1,47 @@
-__version__ = '0.0.1'
+__version__ = '0.0.2'
+
+__all__ = ['is_sorted']
 
 
-def is_sorted(iterable, *, key=None, reverse=False):
+def _compare(reverses, previous, current):
+    # expected that builtin sorted-function/sort-method uses only <
+    # https://docs.python.org/3/howto/sorting.html#odd-and-ends
+    # https://docs.python.org/3/library/stdtypes.html#list.sort
+    for reverse, a, b in zip(reverses, previous, current):
+        if reverse:
+            if a < b:
+                return False
+            if a > b:
+                return True
+        else:
+            if a > b:
+                return False
+            if a < b:
+                return True
+    return True
+
+
+def is_sorted(iterable, *, key=None, reverse=False, multi=None):
     """
     Return True if all items from the iterable are sorted in ascending order.
     Custom sort order can be checked by passing key function.
     Descending order can be checked by setting reverse flag.
     """
-    key = (lambda x: x) if key is None else key
-    # expected that builtin sorted-function/sort-method uses only <
-    # https://docs.python.org/3/howto/sorting.html#odd-and-ends
-    # https://docs.python.org/3/library/stdtypes.html#list.sort
-    if reverse:
-        cmp = lambda a, b: not a < b
-    else:
-        cmp = lambda a, b: not b < a
+    if multi and (key or reverse):
+        raise ValueError('either key/reverse or multi must be specified')
+
+    if not multi:
+        key = (lambda x: x) if not key else key
+        multi = [(key, reverse)]
+    keys, reverses = list(zip(*multi))
 
     is_first = True
     previous = None
-    for current in map(key, iterable):
+    for current in map(lambda x: tuple(k(x) for k in keys), iterable):
         if is_first:
             is_first = False
         else:
-            if not cmp(previous, current):
+            if not _compare(reverses, previous, current):
                 return False
         previous = current
     return True
